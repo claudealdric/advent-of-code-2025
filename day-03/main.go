f2bd68225807e34cde4a1fdb42186ca8d3f32f7a
claudealdric <cvaldric@gmail.com>
2025-12-07 14:56:58 -0500

    Solve day 3 part 2

--- day-03/main.go
+++ day-03/main.go
@@ -15,7 +15,7 @@ func main() {
 
 	var joltageSum int
 	for _, bank := range banks {
-		largestJoltageStr := getLargestJoltage(bank)
+		largestJoltageStr := getLargestJoltage2(bank, 12)
 		largestJoltageInt, err := strconv.Atoi(largestJoltageStr)
 		if err != nil {
 			panic(err)
@@ -51,4 +51,52 @@ func getLargestJoltage(s string) string {
 		}
 	}
 	return largest
+}
+
+func getLargestJoltage2(s string, subsequenceLength int) string {
+	// create a 2D map that stores all the digits and its corresponding index
+	digits := []rune{'9', '8', '7', '6', '5', '4', '3', '2', '1', '0'}
+	m := make(map[rune]map[int]bool)
+	for i, digit := range s {
+		if m[digit] == nil {
+			m[digit] = make(map[int]bool)
+		}
+		m[digit][i] = true
+	}
+
+	indices := make([]int, 0, subsequenceLength)
+	currentLatestIndex := 0
+
+outer:
+	for range subsequenceLength {
+		for _, digit := range digits {
+			for i := currentLatestIndex; i < len(s); i++ {
+				if len(indices) == subsequenceLength {
+					break outer
+				}
+
+				seen := m[digit][i]
+				if !seen {
+					continue
+				}
+
+				remainingChars := len(s) - i
+				neededChars := subsequenceLength - len(indices)
+				if neededChars > remainingChars {
+					continue
+				}
+
+				indices = append(indices, i)
+				currentLatestIndex = i + 1
+				continue outer
+			}
+		}
+	}
+
+	builder := make([]byte, len(indices))
+	for i, e := range indices {
+		builder[i] = s[e]
+	}
+
+	return string(builder)
 }
--- day-03/main_test.go
+++ day-03/main_test.go
@@ -23,3 +23,44 @@ func TestGetLargestJoltage(t *testing.T) {
 		}
 	}
 }
+
+func TestGetLargestJoltage2(t *testing.T) {
+	t.Run("subsequence length of 2", func(t *testing.T) {
+		tests := []struct {
+			input string
+			want  string
+		}{
+			{input: "81119", want: "89"},
+			{input: "98765", want: "98"},
+			{input: "987654321111111", want: "98"},
+			{input: "234234234234278", want: "78"},
+			{input: "818181911112111", want: "92"},
+		}
+
+		for _, test := range tests {
+			got := getLargestJoltage2(test.input, 2)
+			want := test.want
+			if got != want {
+				t.Errorf("got %q, want %q", got, want)
+			}
+		}
+	})
+
+	t.Run("subsequence length of 12", func(t *testing.T) {
+		tests := []struct {
+			input string
+			want  string
+		}{
+			{input: "987654321111111", want: "987654321111"},
+			{input: "811111111111119", want: "811111111119"},
+		}
+
+		for _, test := range tests {
+			got := getLargestJoltage2(test.input, 12)
+			want := test.want
+			if got != want {
+				t.Errorf("got %q, want %q", got, want)
+			}
+		}
+	})
+}
